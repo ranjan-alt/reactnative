794f8e60724afcd8698af6a43ecf10241796b8cc
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-04-16 19:02:53 +0000

    react native setup

---
+++ AwesomeProject/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app ranjan!</Text>
+      <StatusBar style="auto" />
+      <View>
+        <Text>This is an App</Text>
+      </View>
+      <Button title="click me" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
